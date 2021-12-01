46ed886115607506ead448cd7e6e37c1da4a45fe
pauhoms <pauhomsjimenez1@gmail.com>
2021-12-01 23:34:59 +0100

    fix (test): add async method for a memory leaks

--- tests/example.spec.tsx
+++ tests/example.spec.tsx
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 
 import { App } from "../src/App";
 
-describe('Test the <App/> component', () => {
+describe('Test the <App/> component', async () => {
   test('text should be displayed', () => {
     const { getByText, unmount } = render(<App />);
 
