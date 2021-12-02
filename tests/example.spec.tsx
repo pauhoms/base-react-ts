82b600c96da5be3eadc9f2100ce7c1ed405a7c5e
pauhoms <47574509+pauhoms@users.noreply.github.com>
2021-12-02 16:57:15 +0100

    Feature/configurate tests (#1)
    * feat (tests): install libs for configurate the tests
    
    * feat (tests): add new libs and scripts
    
    * feat (tests): create config files for run tests
    
    * feat (tests): add example tests
    
    * feat (tests): add tests inside pipeline:
    
    * fix (test): typo
    
    * fix (test): typo
    
    * fix (test): command in the pipeline
    
    * refactor (tests): add the docker command in pipeline
    
    * fix (test): unmount component for a memory leaks
    
    * fix (test): add async method for a memory leaks
    
    * DEBUG
    
    * DEBUG
    
    * DEBUG
    
    * DEBUG CI
    
    * fix: naming

---
+++ tests/example.spec.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+import { App } from "../src/App";
+
+describe('Test the <App/> component', () => {
+  test('text should be displayed', () => {
+    const { getByText, unmount } = render(<App />);
+
+    expect(getByText('Hello component !')).toBeInTheDocument();
+    unmount();
+  });
+});
